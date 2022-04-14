d6c9e34c68db7f8d7e0decf94c0befd5acfabf9e
Cristhofer Parada <cristhofer.parada@usach.cl>
2022-04-13 23:20:21 -0400

    [Cristhofer Parada] - Wipeout del repositorio de nuevo

--- Backend/src/main/java/grupo1/demo/repositories/DogRepository.java
+++
@@ -1,8 +0,0 @@
-package grupo1.demo.repositories;
-
-import grupo1.demo.models.Dog;
-
-//Interfaz de Dog
-public interface DogRepository {
-    public int countDogs();
-}
--- Frontend/pages/acerca-de.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <div class="container">
-        <h1><center>Este es el Control 2 de TBD</center> </h1>
-        <p><center> Grupo 1 - TBD - 2022</center></p>
-        
-    </div>
-</template>
